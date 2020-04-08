54f4effad19ff1f1c93249a0b593025bcdb2c108
mile662 <1585633@qq.com>
2020-04-08 22:09:01 +0800

    修改：     jump7.py

--- jump7.py
+++ jump7.py
@@ -1,8 +1,8 @@
 a=1
-while a<=100
-   if (a%7=0 or (a//10=7 or a%7=7))
+while a <= 100 :
+    if (a % 7 == 0 or (a // 10 == 7 or a % 7 == 7)) :
         print(a)
         a=a+1
-    else
+    else:
         a=a+1
 
